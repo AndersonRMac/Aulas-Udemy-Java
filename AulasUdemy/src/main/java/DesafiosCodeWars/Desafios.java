b95e92483dce58adf17219a2a413c56638458887
Your Name <anderson.ramosmaciel@gmail.com>
2025-07-11 13:49:25 -0300

    Resolvi mais um desafio de java.

--- AulasUdemy/src/main/java/DesafiosCodeWars/Desafios.java
+++ AulasUdemy/src/main/java/DesafiosCodeWars/Desafios.java
@@ -5,13 +5,24 @@ public class Desafios {
 
     }
 
-    //Complete a função de soma quadrada para que ela eleve ao quadrado cada número passado a ela e então some os resultados.
+    //Complete a função de soma quadrada para que ela eleve ao quadrado cada número
+    // passado a ela e então some os resultados.
     public int SquareSum(int[] arr) {
         int resultado = 0;
         for (int i = 0; i < arr.length; i++) {
             resultado += arr[i] * arr[i];
         }
         return resultado;
+    }
+    //Se uma das flores tiver um número par de pétalas e a outra um número ímpar, significa que estão apaixonados.
+    //Escreva uma função que receberá o número de pétalas de cada flor e retornará true se elas
+    // estiverem apaixonadas e false se não estiverem.
+    public static boolean isLove(final int flower1, final int flower2) {
+        boolean resultado;
+        if ((flower1 + flower2) % 2 == 0) {
+            return resultado = false;
+        } else
+            return resultado = true;
     }
 }
 
